8112ee5eaf148d711590ec5855344d9ac1c4c1e2
薛丞宏 <ihcaoe@gmail.com>
2018-02-01 11:57:39 +0800

    加試驗

---
+++ test試驗.py
@@ -0,0 +1,25 @@
+from unittest.case import TestCase
+from 轉trs.轉人工 import 轉
+
+
+class 轉音試驗(TestCase):
+    def test_內底(self):
+        原本 = '''<Sync time="408.335"/>
+di3 e1 gong1 te2-gong1
+<Event desc="giong2" type="lexical" extent="previous"/>
+-zia4
+<Sync time="409.946"/>'''
+        後來 = '''<Sync time="408.335"/>
+di3 e1 gong1 te2-gong1
+<Event desc="kiong7" type="lexical" extent="previous"/>
+-zia4
+<Sync time="409.946"/>'''
+        self.assertEqual(轉('').轉內底(原本), 後來)
+    def test_外口(self):
+        原本 = '''<Sync time="408.335"/>
+zit7-e3 za2-bo2-lang5
+<Sync time="409.946"/>'''
+        後來 = '''<Sync time="408.335"/>
+tsit4-e3 tsa7-poo7-lang5
+<Sync time="409.946"/>'''
+        self.assertEqual(轉('').轉外口(原本), 後來)
--- 轉trs/轉人工.py
+++ 轉trs/轉人工.py
@@ -25,6 +25,9 @@ class 轉:
                 )
 
     def 通用轉臺羅(self, 聽拍):
+        return self.轉外口(self.轉內底(聽拍))
+
+    def 轉外口(self, 聽拍):
         結果 = []
         for 第幾筆, 羅馬字 in enumerate(self.揣音標.split(聽拍)):
             if 第幾筆 % 2 == 0:
@@ -37,16 +40,28 @@ class 轉:
                     結果.append(羅馬字)
         return ''.join(結果)
 
+    def 轉內底(self, 聽拍):
+        return re.sub(r'(<Event[^/]*type="lexical"[^/]*/>)', self.轉event的lexicon, 聽拍)
 
-專案 = join(dirname(__file__), '..')
-for 所在, _, 檔名陣列 in walk(join(專案, '分享')):
-    for 檔名 in 檔名陣列:
-        資料夾名 = basename(所在)
-        if 資料夾名 != '臺羅trs' and 檔名.endswith('.trs'):
-            通用trs檔名 = join(所在, 檔名)
-            臺羅trs檔名 = join(專案, '臺羅', 資料夾名, '臺羅trs', 檔名)
-            makedirs(join(dirname(臺羅trs檔名)), exist_ok=True)
-            with open(臺羅trs檔名, 'wt') as 臺羅檔案:
-                parser = 轉(臺羅檔案)
-                with open(通用trs檔名, 'rt') as 通用檔案:
-                    parser.處理(通用檔案.read())
+    def 轉event的lexicon(self, event):
+        tshue = re.split(r'(desc="([a-z]+\d)")', event.group(0))
+        臺羅 = 通用拼音音標(tshue[2]).轉換到臺灣閩南語羅馬字拼音()
+        if not 臺羅:
+            臺羅 = tshue[2]
+        結果 = '{}desc="{}"{}'.format(tshue[0], 臺羅, tshue[-1])
+        return 結果
+
+
+if __name__ == '__main__':
+    專案 = join(dirname(__file__), '..')
+    for 所在, _, 檔名陣列 in walk(join(專案, '分享')):
+        for 檔名 in 檔名陣列:
+            資料夾名 = basename(所在)
+            if 資料夾名 != '臺羅trs' and 檔名.endswith('.trs'):
+                通用trs檔名 = join(所在, 檔名)
+                臺羅trs檔名 = join(專案, '臺羅', 資料夾名, '臺羅trs', 檔名)
+                makedirs(join(dirname(臺羅trs檔名)), exist_ok=True)
+                with open(臺羅trs檔名, 'wt') as 臺羅檔案:
+                    parser = 轉(臺羅檔案)
+                    with open(通用trs檔名, 'rt') as 通用檔案:
+                        parser.處理(通用檔案.read())
