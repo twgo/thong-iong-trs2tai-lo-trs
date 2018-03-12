a3d62c9be50b0f9edb7434fef0712ecb28234db0
薛丞宏 <ihcaoe@gmail.com>
2018-03-12 14:40:15 +0800

    漢字有接起來矣，毋過有timeout的問題
    /%E5%8F%A3%E8%AA%9E%E6%A8%99%E6%BC%A2%E5%AD%97%E6%9C%AC%E8%AA%BF/u3-e7%20si3%20ti3%20too7-tsai1-tiunn5%20to3%20si3%20kong1%20ti7-tsau3
    502 Bad Gateway
    u3-e7 si3 ti3 too7-tsai1-tiunn5 to3 si3 kong1 ti7-tsau3

--- 輸出入.py
+++ 輸出入.py
@@ -1,5 +1,29 @@
 from 改資料.輸出入 import 讀資料
 from 轉trs.轉人工 import 轉
+from http.client import HTTPSConnection
+import json
+from urllib.parse import quote
+
+
+class 通用轉漢字臺羅:
+    @classmethod
+    def 揣漢字(cls, 音標):
+        if 音標.strip()=='':
+            return ''
+        conn = HTTPSConnection("twisas.iis.sinica.edu.tw")
+        網址 = (
+            "/%E5%8F%A3%E8%AA%9E%E6%A8%99%E6%BC%A2%E5%AD%97%E6%9C%AC%E8%AA%BF/" +
+            quote(音標)
+        )
+        conn.request("GET", 網址)
+        r1 = conn.getresponse()
+        if r1.status != 200:
+            print(網址)
+            print(r1.status, r1.reason)
+            print(音標)
+            raise RuntimeError()
+        data1 = r1.read()  # This will return entire content.
+        return json.loads(data1.decode('utf-8'))['漢字']
 
 
 if __name__ == '__main__':
@@ -9,15 +33,15 @@ if __name__ == '__main__':
             if isinstance(一筆, str):
                 print(一筆)
             else:
-                這句=[]
+                這句 = []
                 for 一个 in 一筆:
                     if 一个.startswith('<Event'):
                         這句.append(讀資料.event換掉(一个))
                     else:
                         這句.append(一个)
-                原本=''.join(這句).strip()
-                通用=原本
-                print('漢字：{}'.format(原本))
-                print('臺羅：{}'.format(轉(None).轉外口(原本)))
+                原本 = ''.join(這句).strip()
+                臺羅 = 轉(None).轉外口(原本)
+                print('漢字：{}'.format(通用轉漢字臺羅.揣漢字(臺羅)))
+                print('臺羅：{}'.format(臺羅))
                 print('通用：{}'.format(原本))
                 print('原本：{}'.format(原本))
