78c7d54ef6576f3d1cc99667e4d9635842a1f6da
薛丞宏 <ihcaoe@gmail.com>
2018-03-12 11:28:28 +0800

    做好noise的部份

--- 改資料/輸出入.py
+++ 改資料/輸出入.py
@@ -1,3 +1,5 @@
+import re
+
 
 class 讀資料:
     @classmethod
@@ -20,3 +22,23 @@ class 讀資料:
         if 一逝.startswith('<Event'):
             return True
         return False
+
+    @classmethod
+    def event換掉(cls, 一逝):
+        拍毋著字 = {
+            'silencd': 'silence',
+            'sile3nce': 'silence',
+            'sience': 'silence',
+            'silecne': 'silence',
+            'noisy': 'noise',
+            'silecne': 'noise',
+            'nooise': 'noise',
+            'npoise': 'noise',
+            'emtpy': 'empty',
+            'emty': 'empty',
+        }
+        if 'type="noise"' in 一逝:
+            型態 = re.search('desc="(.+?)"', 一逝).group(1)
+            if 型態 in 拍毋著字:
+                型態 = 拍毋著字[型態]
+            return ' {} '.format(型態.upper())
