bac868cad0c545d8a5776822405dbef3798eda18
薛丞宏 <ihcaoe@gmail.com>
2018-03-13 16:56:47 +0800

    加host

--- tsuan2/settings.py
+++ tsuan2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v(ye-s1@a5@gg32ftw_r*%qtz+wfx_#u0luzyesqm$p3p3m=m!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
